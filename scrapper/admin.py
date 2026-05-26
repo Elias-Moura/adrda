85b2c9efcf6748c30161e018e4111a192c329e0f
Elias Moura <elias.smourayt@gmail.com>
2026-05-26 18:46:00 -0300

    refactor(admin): admin do Ativo unificado

--- scrapper/admin.py
+++ scrapper/admin.py
@@ -1,4 +1,3 @@
-# TODO(Task-7): reescrever admin para Ativo unificado
 from django.contrib import admin
 
 from .models import Ativo, CotacaoDiaria, Job
@@ -6,9 +5,10 @@ from .models import Ativo, CotacaoDiaria, Job
 
 @admin.register(Ativo)
 class AtivoAdmin(admin.ModelAdmin):
-    list_display = ["nome", "tipo", "cnpj", "ticker", "gestora", "criado_em"]
-    search_fields = ["nome", "cnpj", "ticker"]
-    list_filter = ["tipo"]
+    list_display = ["nome", "tipo", "subtipo", "ticker", "cnpj", "gestora", "atualizado_em"]
+    list_filter = ["tipo", "subtipo"]
+    search_fields = ["nome", "cnpj", "ticker", "id_quantum"]
+    readonly_fields = ["criado_em", "atualizado_em"]
 
 
 @admin.register(CotacaoDiaria)
